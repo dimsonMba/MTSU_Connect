bb65052991ab248bed28b02cfd05035b5723eb89
Dimitri Nanmejo <dimitrisinou@gmail.com>
2026-01-31 21:01:22 -0600

    Adding some fix

--- MobileApp/babel.config.js
+++ MobileApp/babel.config.js
@@ -1,7 +1,12 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
-    plugins: ["nativewind/babel"],
+    presets: [
+      ["babel-preset-expo", { unstable_transformImportMeta: true }],
+      // nativewind/babel exports a preset-like object (it defines .plugins),
+      // so it must be added to `presets` rather than `plugins` to avoid
+      // Babel validation errors.
+      require("nativewind/babel"),
+    ],
   };
 };
